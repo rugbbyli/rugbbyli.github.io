959a90da7e6e36f0cfc5169e6cec766a2a0c15a5
RugbbyLi <19198926@qq.com>
2014-10-10 21:57:47 +0800

    -update

--- files/ImageButton.cs
+++ files/ImageButton.cs
@@ -1,85 +1,75 @@
-﻿//using UnityEngine;
-//using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine;
 
-////----------------------------------------------
-////            NGUI: Next-Gen UI kit
-//// Copyright © 2011-2014 Tasharen Entertainment
-////----------------------------------------------
+[ExecuteInEditMode]
+[AddComponentMenu("UI/ImageButton")]
+public class ImageButton : UI2DSprite
+{
+    //缓存按钮4种状态下的Sprite
+    public Sprite NormalSprite;
+    public Sprite HoverSprite;
+    public Sprite PressedSprite;
+    public Sprite DisabledSprite;
 
-//using UnityEngine;
+    /// <summary>
+    /// 获取或设置按钮的可用性。注：按钮必须包含一个Collider才能触发输入事件。
+    /// </summary>
+    public bool isEnabled
+    {
+        get
+        {
+            var col = collider;
+            return col && col.enabled;
+        }
+        set
+        {
+            var col = collider;
+            if (!col) return;
 
-///// <summary>
-///// Sample script showing how easy it is to implement a standard button that swaps sprites.
-///// </summary>
-//[ExecuteInEditMode]
-//[AddComponentMenu("UI/ImageButton")]
-//public class ImageButton : UI2DSprite
-//{
-//    //缓存按钮4种状态下的Sprite
-//    public Sprite NormalSprite;
-//    public Sprite HoverSprite;
-//    public Sprite PressedSprite;
-//    public Sprite DisabledSprite;
+            if (col.enabled != value)
+            {
+                col.enabled = value;
+                UpdateImage();
+            }
+        }
+    }
 
-//    /// <summary>
-//    /// 获取或设置按钮的可用性。注：按钮必须包含一个Collider才能触发输入事件。
-//    /// </summary>
-//    public bool isEnabled
-//    {
-//        get
-//        {
-//            var col = collider;
-//            //return col == null ? (target.collider2D && target.collider2D.enabled) : (col && col.enabled);
-//            return col && col.enabled;
-//        }
-//        set
-//        {
-//            var col = collider;
-//            if (!col) return;
+    void OnEnable()
+    {
+        UpdateImage();
+    }
 
-//            if (col.enabled != value)
-//            {
-//                col.enabled = value;
-//                UpdateImage();
-//            }
-//        }
-//    }
+    void OnValidate()
+    {
+        if (NormalSprite == null) NormalSprite = sprite2D;
+        if (HoverSprite == null) HoverSprite = sprite2D;
+        if (PressedSprite == null) PressedSprite = sprite2D;
+        if (DisabledSprite == null) DisabledSprite = sprite2D;
+    }
 
-//    void OnEnable()
-//    {
-//        UpdateImage();
-//    }
+    void UpdateImage()
+    {
+        if (isEnabled) SetSprite(UICamera.IsHighlighted(gameObject) ? HoverSprite : NormalSprite);
+        else SetSprite(DisabledSprite);
+    }
 
-//    void OnValidate()
-//    {
-//        if (NormalSprite == null) NormalSprite = sprite2D;
-//        if (HoverSprite == null) HoverSprite = sprite2D;
-//        if (PressedSprite == null) PressedSprite = sprite2D;
-//        if (DisabledSprite == null) DisabledSprite = sprite2D;
-//    }
+    void OnHover(bool isOver)
+    {
+        if (isEnabled)
+            SetSprite(isOver ? HoverSprite : NormalSprite);
+    }
 
-//    void UpdateImage()
-//    {
-//        if (isEnabled) SetSprite(UICamera.IsHighlighted(gameObject) ? HoverSprite : NormalSprite);
-//        else SetSprite(DisabledSprite);
-//    }
+    void OnPress(bool pressed)
+    {
+        if (pressed) SetSprite(PressedSprite);
+        else UpdateImage();
+    }
 
-//    void OnHover(bool isOver)
-//    {
-//        if (isEnabled)
-//            SetSprite(isOver ? HoverSprite : NormalSprite);
-//    }
-
-//    void OnPress(bool pressed)
-//    {
-//        if (pressed) SetSprite(PressedSprite);
-//        else UpdateImage();
-//    }
-
-//    void SetSprite(Sprite sprite)
-//    {
-//        sprite2D = sprite;
-//        //this.SetDirty();
-//    }
-//}
+    void SetSprite(Sprite sprite)
+    {
+        sprite2D = sprite;
+        //this.SetDirty();
+    }
+}
 
